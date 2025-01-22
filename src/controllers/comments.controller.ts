c55620b0002dd78930045374208c56f8fe2a7dbf
Ron Miles <ron@lumia.security>
2025-01-22 21:49:17 +0200

    feat(comments): Add controller and get by post id function

---
+++ src/controllers/comments.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+import commentModel, { IComment } from "../models/comment.model";
+import BaseController from "./base.controller";
+
+class CommentsController extends BaseController<IComment> {
+  constructor() {
+    super(commentModel);
+  }
+
+  async getAllCommentsByPostId(req: Request, res: Response) {
+    try {
+      const comments = await this.model.find({ postId: req.query.postId });
+
+      res.send(comments);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+
+}
+
+export default new CommentsController();
