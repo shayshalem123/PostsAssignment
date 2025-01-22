020c4e3703879583f0f86ff61eafa11cfe2b3cff
Ron Miles <ron.miles2802@gmail.com>
2025-01-22 22:50:16 +0200

    feat(comments): add update request

--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -8,12 +8,12 @@ router.get("/", commentsController.getAll.bind(commentsController));
 
 router.post("/", commentsController.create.bind(commentsController));
 
-router.get("/:id", commentsController.getById.bind(commentsController));
-
-router.get("/post", commentsController.getAllCommentsByPostId.bind(commentsController));
-
 router.get("/post", commentsController.getAllCommentsByPostId.bind(commentsController));
 
 router.get("/:id", commentsController.getById.bind(commentsController));
+
+router.delete("/:id", commentsController.deleteItem.bind(commentsController));
+
+router.put("/:id", commentsController.update.bind(commentsController));
 
 export default router;
