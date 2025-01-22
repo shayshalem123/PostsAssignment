aac2466c3a5ed64c7fe5c7587897fc1335937eb5
shayshalem <shshai007@gmail.com>
2025-01-22 21:39:48 +0200

    feat(posts): change posts

--- src/routes/posts.route.ts
+++ src/routes/posts.route.ts
@@ -4,12 +4,18 @@ import postsController from "../controllers/posts.controller";
 
 const router = express.Router();
 
-router.get("/", postsController.getAll.bind(postsController));
+router.get("/", (req, res) => {
+  if (req.query.sender) {
+    postsController.getAllPostsBySender(req, res);
+  } else {
+    postsController.getAll(req, res);
+  }
+});
 
 router.post("/", postsController.create.bind(postsController));
 
-router.get("/:id", postsController.getById.bind(postsController));
-
 router.get("/post", postsController.getAllPostsBySender.bind(postsController));
+
+router.get("/:id", postsController.getById.bind(postsController));
 
 export default router;
