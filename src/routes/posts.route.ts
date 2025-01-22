b723ac7f25a132c11c0957817f50c850112c972c
shayshalem <shshai007@gmail.com>
2025-01-22 21:01:53 +0200

    feat(posts): add getAll & getById

--- src/routes/posts.route.ts
+++ src/routes/posts.route.ts
@@ -4,6 +4,10 @@ import postsController from "../controllers/posts.controller";
 
 const router = express.Router();
 
+router.get("/", postsController.getAll.bind(postsController));
+
 router.post("/", postsController.create.bind(postsController));
+
+router.get("/:id", postsController.getById.bind(postsController));
 
 export default router;
